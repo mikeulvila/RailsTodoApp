84586e2e6a0c4006cc2d5e4b3245fe9d87e99e5e
Mike Ulvila <mikeulvila@me.com>
2016-04-28 13:01:36 -0500

    added first rspec test

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "Creating todo lists" do
+  it "redirects to the todo list index page on success" do
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New todo_list")
+  end
+end
