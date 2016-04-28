a773fb997346555c9390a125701d9a87c280011d
Mike Ulvila <mikeulvila@me.com>
2016-04-28 13:56:48 -0500

    more tests and validation for title on todo list

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -4,6 +4,30 @@ describe "Creating todo lists" do
   it "redirects to the todo list index page on success" do
     visit "/todo_lists"
     click_link "New Todo list"
-    expect(page).to have_content("New todo_list")
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "My todo list"
+    fill_in "Description", with: "Create todo list"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("My todo list")
+  end
+
+  it "Displays an error when the todo list has no title" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: ""
+    fill_in "Description", with: "This is what I'm doing today"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today")
   end
 end
