2f41cdd85e752c3fb4dfde2cef9af7ebea42d9c1
Mike Ulvila <mikeulvila@me.com>
2016-05-05 14:35:10 -0500

    show if list has completed items

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,8 +1,13 @@
 class TodoList < ActiveRecord::Base
   has_many :todo_items
-  
+
   validates :title, presence: true
   validates :title, length: { minimum: 3 }
   validates :description, presence: true
   validates :description, length: { minimum: 5 }
+
+  def has_completed_items?
+    todo_items.complete.size > 0
+  end
+
 end
