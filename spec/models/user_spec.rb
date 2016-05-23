9d87bc12c368885277ebe4678599d2a8b66954e7
Mike Ulvila <mikeulvila@me.com>
2016-05-23 12:43:51 -0500

    user model validates email presence true

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) {
+    {
+      first_name: "Mike",
+      last_name: "Ulvila",
+      email: "mike@mike.com"
+    }
+  }
+  context "validations" do
+    let(:user) { User.new(valid_attributes) }
+    it "requires an email" do
+      expect(user).to validate_presence_of(:email)
+    end
+  end
 end
