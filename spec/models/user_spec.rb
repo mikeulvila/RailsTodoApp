eefddae8ccb63bf311ef8de4934d19f012be4987
Mike Ulvila <mikeulvila@me.com>
2016-05-23 12:46:52 -0500

    validates uniqueness of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,14 @@ describe User do
   }
   context "validations" do
     let(:user) { User.new(valid_attributes) }
+
     it "requires an email" do
       expect(user).to validate_presence_of(:email)
     end
+
+    it "requires a unique email" do
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
   end
 end
