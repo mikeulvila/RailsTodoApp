7f7dcd1f9a39142b17c0aa7145203088a8c87730
Mike Ulvila <mikeulvila@me.com>
2016-05-25 16:04:46 -0500

    logs the user in and goes to todo lists

--- spec/features/users/authentication_spec.rb
+++ spec/features/users/authentication_spec.rb
@@ -1,6 +1,18 @@
 require "spec_helper"
 
 describe "Logging In" do
+  it "logs the user in and goes to the todo lists" do
+    User.create(first_name: "Mike", last_name: "Ulvila",
+                              email: "mike@mike.com", password: "password1234",
+                              password_confirmation: "password1234")
+    visit new_user_session_path
+    fill_in "Email", with: "mike@mike.com"
+    fill_in "Password", with: "password1234"
+    click_button "Log In"
+    expect(page).to have_content("Todo Lists")
+    expect(page).to have_content("Thanks for logging in!")
+  end
+
   it "displays an email address in the event of a failed login" do
     visit new_user_session_path
     fill_in "Email", with: "mike@mike.com"
