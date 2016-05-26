64cfdfa2f7d7e2019eb8efaa3276fde580108e2e
Mike Ulvila <mikeulvila@me.com>
2016-05-26 12:15:58 -0500

    added user factory for testing

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "First"
+    last_name "Last"
+    sequence(:email) { |n| "user#{n}@odot.com" }
+    password "password1234"
+    password_confirmation "password1234"
+  end
+end
