b76e7272b972e87e676433542dabab71301e0915
Mike Ulvila <mikeulvila@me.com>
2016-05-26 12:07:10 -0500

    include FactoryGirl in spec_helper config block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include TodoListHelpers, type: :feature
   config.include RailsDomIdHelper, type: :feature
+  config.include FactoryGirl::Syntax::Methods
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
