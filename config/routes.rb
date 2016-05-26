6160ceacd3f64d0afe2421d6fa43b6f7f2e2fb1f
Mike Ulvila <mikeulvila@me.com>
2016-05-26 12:45:17 -0500

    add Feature sub module to AuthHelpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-
+  get "/login" => "user_sessions#new", as: :login
+  delete "/logout" => "user_sessions#destroy", as: :logout
+  
   resources :users
   resources :user_sessions, only: [:new, :create]
-  
+
   resources :todo_lists do
     resources :todo_items do
       member do
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Creating todo lists" do
-
+  let(:user) { create(:user) }
   def create_todo_list(options={})
       options[:title] ||= "My todo list"
       options[:description] ||= "This is my todo list"
@@ -13,6 +13,10 @@ describe "Creating todo lists" do
       fill_in "Title", with: options[:title]
       fill_in "Description", with: options[:description]
       click_button "Create Todo list"
+  end
+
+  before do
+    sign_in user, password: "password1234"
   end
 
   it "redirects to the todo list index page on success" do
