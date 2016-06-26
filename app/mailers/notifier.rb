52e19fd0e2fe332ee19f012e08f3ef67981c86a7
Mike Ulvila <mikeulvila@me.com>
2016-06-25 22:41:53 -0500

    generate mailer notifier

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,9 @@
+class Notifier < ApplicationMailer
+  default_url_options[:host] = "localhost:3000"
+
+  def password_reset (user)
+    @user = user
+    mail(to: "#{user.first_name} #{user.last_name} <#{user.email}>",
+         subject: "Reset Your Password")
+  end
+end
